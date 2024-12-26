f26e680a8e2c0be8350c708bb68b021218682d7e
zhujiadong <826695779@qq.com>
2024-12-26 14:46:36 +0800

    expose register_cfg_parser decorator

--- cfgengine/__init__.py
+++ cfgengine/__init__.py
@@ -1,5 +1,5 @@
 from .config_loader import ConfigLoader
 from .default_parser import INIParser, JSONParser
-from .parser_registry import CfgParser, ParserRegistry
+from .parser_registry import CfgParser, ParserRegistry, register_cfg_parser
 
-__all__ = ["CfgParser", "ParserRegistry", "ConfigLoader"]
+__all__ = ["CfgParser", "ParserRegistry", "ConfigLoader", "register_cfg_parser"]
